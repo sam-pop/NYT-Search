0d4e965a50ccd0dbfc30caa8059c3da0698afffb
sam-pop <poplovitch.samuel@gmail.com>
2018-06-01 14:24:41 -0400

    js update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,19 +1,34 @@
 var apiKey = "f551d639ab834af98e5a90d095b18886";
 var searchParam = "google";
+var numRecords;
 var urlAPI = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchParam;
 
-$.ajax({
-    url: urlAPI,
-    method: "GET",
-    success: function (res) {
-        console.log(res);
-        for (var i = 0; i < res.response.docs.length; i++) {
-            var title = res.response.docs[i].headline.main;
-            var snippet = res.response.docs[i].snippet;
-            console.log(title);
+
+
+function getSearchParam() {
+    searchParam = $('#exampleFormControlInput1').val();
+}
+
+function getRecordNum() {
+    numRecords = $('exampleFormControlSelect1"').val();
+}
+
+function runApi() {
+    $.ajax({
+        url: urlAPI,
+        method: "GET",
+        success: function (res) {
+            for (var i = 0; i < numRecords; i++) {
+                var title = res.response.docs[i].headline.main;
+                var snippet = res.response.docs[i].snippet;
+            }
+        },
+        error: function () {
+            console.log('API ERROR');
         }
-    },
-    error: function () {
-        console.log('API ERROR');
-    }
+    });
+}
+
+$(function () {
+
 });
