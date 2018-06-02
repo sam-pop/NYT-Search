d26e29af837a10ea7e837509d5430c517b99578c
sam-pop <poplovitch.samuel@gmail.com>
2018-06-02 17:16:59 -0400

    js update

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,25 +1,41 @@
-var apiKey = "f551d639ab834af98e5a90d095b18886";
+const API_KEY = "f551d639ab834af98e5a90d095b18886";
+var searchParam;
+var numRecords;
+
+function getSearchParam() {
+    let $search = $('#exampleFormControlInput1');
+    return $search.val().trim();
+}
+
+function getNumArticles() {
+    let $numArticles = $('#exampleFormControlSelect1');
+    return $numArticles.val().trim();
+}
+
+function showResults(res) {
+    $('.results').empty();
+
+    for (var i = 0; i < getNumArticles(); i++) {
+        var title = res.response.docs[i].headline.main;
+        var snippet = res.response.docs[i].snippet;
+        var urls = res.response.docs[i].web_url;
+        $('.results').append($('<div>').text(title));
+        $('.results').append($('<div>').text("--" + snippet));
+        $('.results').append($('<a>').attr('href', urls).text('Link'));
+    }
+
+}
 
 
 function runApi() {
-    $('.results').empty();
-    var searchParam = $('#exampleFormControlInput1').val().trim();
-    var numRecords = $('#exampleFormControlSelect1').val().trim();
+    searchParam = getSearchParam();
+    var urlAPI = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + API_KEY + "&q=" + searchParam;
 
-    var urlAPI = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" + apiKey + "&q=" + searchParam;
     $.ajax({
         url: urlAPI,
         method: "GET",
         success: function (res) {
-
-            for (var i = 0; i < numRecords; i++) {
-                var title = res.response.docs[i].headline.main;
-                var snippet = res.response.docs[i].snippet;
-                var urls = res.response.docs[i].web_url;
-                $('.results').append($('<div>').text(title));
-                $('.results').append($('<div>').text("--" + snippet));
-                $('.results').append($('<a>').attr('href', urls).text('Link'));
-            }
+            showResults(res);
         },
         error: function () {
             console.log('API ERROR');
